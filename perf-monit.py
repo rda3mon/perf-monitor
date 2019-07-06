864c356e5ede8cb6560200e139256ef3a0e764dd
rda3mon <mallikvarjun@gmail.com>
2019-07-06 21:43:46 +0530

    plot changes added

--- perf-monit.py
+++ perf-monit.py
@@ -3,13 +3,16 @@
 import argparse
 import time
 import subprocess
-import datetime
+import os
+import matplotlib.pyplot as plt
 
 fetch_cpu = "ps -p {} -o %cpu --no-headers"
 fetch_mem = "ps -p {} -o %mem --no-headers"
 fetch_threads = "ps -p {} -o nlwp --no-headers"
 fetch_lsof = "lsof -p {} 2> /dev/null | wc -l"
 fetch_load = "cat /proc/loadavg"
+
+default_dir = "/tmp/perf-monit"
 
 def run_command(command):
     try:
@@ -29,24 +32,31 @@ def writeToFile(content, fp, close=False):
     else:
         fp.close()
 
-def monit(pid, output, interval, outfile):
+def monit(pid, output, interval, dirpath, draw):
+    try:
+        os.makedirs(dirpath)
+    except:
+        pass
     if output == "csv":
-        fp = open(outfile, "a");
+        fp = open("{}/{}.csv".format(dirpath, pid), "a");
         writeToFile("Time,CPU,Memory,Threads,Open Files,System Load\n", fp)
 
+    plot_out = {"cpu": [], "mem": [], "threads": [], "lsof": [], "load": [], "now": []}
     try: 
+        now = 0;
         while True:
-            now = datetime.datetime.now();
-            cpu = run_command(fetch_cpu.format(pid));
-            mem = run_command(fetch_mem.format(pid));
-            threads = run_command(fetch_threads.format(pid));
-            lsof = run_command(fetch_lsof.format(pid));
-            load = run_command(fetch_load);
+            plot_out["cpu"].append(run_command(fetch_cpu.format(pid)))
+            plot_out["mem"].append(run_command(fetch_mem.format(pid)))
+            plot_out["threads"].append(run_command(fetch_threads.format(pid)))
+            plot_out["lsof"].append(run_command(fetch_lsof.format(pid)))
+            plot_out["load"].append(run_command(fetch_load))
+            plot_out["now"].append(now)
 
             if output == "csv":
-                writeToFile("{},{},{},{},{},{}\n".format(now, cpu, mem, threads, lsof, load), fp);
+                writeToFile("{},{},{},{},{},{}\n".format(plot_out["now"][-1], plot_out["cpu"][-1], plot_out["mem"][-1], plot_out["threads"][-1], plot_out["lsof"][-1], plot_out["load"][-1]), fp);
             else:
-                print("Time: {}\nCPU: {}\nMemory: {}\nThreads: {}\nOpen Files: {}\nLoad Factor: {}\n\n".format(now, cpu, mem, threads, lsof, load));
+                print("Time: {}\nCPU: {}\nMemory: {}\nThreads: {}\nOpen Files: {}\nLoad Factor: {}\n\n".format(plot_out["now"][-1], plot_out["cpu"][-1], plot_out["mem"][-1], plot_out["threads"][-1], plot_out["lsof"][-1], plot_out["load"][-1]));
+            now += interval;
             time.sleep(interval);
     except KeyboardInterrupt as ex:
         print("Keyboard interrupted. Closing file");
@@ -56,14 +66,29 @@ def monit(pid, output, interval, outfile):
         if output == "csv":
             writeToFile(None, fp, True);
 
+        fig, axs = plt.subplots(figsize=(24,16), nrows=2, ncols=2)
 
+        axs[0, 0].plot(plot_out["now"], list(map(float, plot_out["cpu"])), 'tab:orange')
+        axs[0, 0].set_title('CPU')
+        axs[0, 1].plot(plot_out["now"], list(map(float, plot_out["lsof"])), 'tab:green')
+        axs[0, 1].set_title('Open Files')
+        axs[1, 0].plot(plot_out["now"], list(map(float, plot_out["mem"])), 'tab:red')
+        axs[1, 0].set_title('Memory')
+        axs[1, 1].plot(plot_out["now"], list(map(float, plot_out["threads"])), 'tab:blue')
+        axs[1, 1].set_title('Threads')
+
+        for ax in axs.flat:
+            ax.set(xlabel='Time')
+
+        plt.savefig("{}/{}.svg".format(dirpath, pid), format='svg', dpi=1200, bbox_inches='tight')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=False,description='Performance Monitoring tools');
     parser.add_argument("-p", "--pid", help="Process id for analysing the performance", required=True);
     parser.add_argument("-o", "--output", choices=['csv', 'raw'], help="Output format", default="raw");
     parser.add_argument("-i", "--interval", choices=[3, 5, 10, 20, 60], type=int, help="Monitor interval", default=3);
-    parser.add_argument("-f", "--outfile", default="/tmp/perf-monit.csv", help="Monitor interval");
+    parser.add_argument("-s", "--dirpath", default=default_dir, help="Directory path where files to be stored");
+    parser.add_argument("-d", "--draw", action='store_true', help="Store the output as figures");
     args = parser.parse_args();
 
-    monit(args.pid, args.output, args.interval, args.outfile);
+    monit(args.pid, args.output, args.interval, args.dirpath, args.draw);
