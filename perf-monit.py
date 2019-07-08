6a61b3abf096b7467e8cb477857a96557971d5ed
rda3mon <mallikvarjun@gmail.com>
2019-07-08 08:23:22 +0530

    minor refactoring

--- perf-monit.py
+++ perf-monit.py
@@ -5,6 +5,7 @@ import time
 import subprocess
 import os
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 fetch_cpu = "ps -p {} -o %cpu --no-headers"
 fetch_mem = "ps -p {} -o %mem --no-headers"
@@ -24,13 +25,21 @@ def run_command(command):
             output = 0
     except Exception as e:
         print("An exception occurred while executing command. Message: " + e.message);
-    return output 
+    return output
 
 def writeToFile(content, fp, close=False):
     if close is False:
         fp.write(content)
     else:
         fp.close()
+
+def draw_subplot(plot, color, x, y, xlabel, ylabel):
+    x = list(map(float, x))
+    y = list(map(float, y))
+    plot.plot(x, y, color)
+    #plot.set_title(ylabel)
+    #plot.set_yticks(np.arange(min(y), max(y), 1));
+    plot.set(xlabel=xlabel, ylabel=ylabel)
 
 def monit(pid, output, interval, dirpath, draw):
     try:
@@ -42,7 +51,7 @@ def monit(pid, output, interval, dirpath, draw):
         writeToFile("Time,CPU,Memory,Threads,Open Files,System Load\n", fp)
 
     plot_out = {"cpu": [], "mem": [], "threads": [], "lsof": [], "load": [], "now": []}
-    try: 
+    try:
         now = 0;
         while True:
             plot_out["cpu"].append(run_command(fetch_cpu.format(pid)))
@@ -66,23 +75,19 @@ def monit(pid, output, interval, dirpath, draw):
         if output == "csv":
             writeToFile(None, fp, True);
 
-        fig, axs = plt.subplots(figsize=(24,16), nrows=3, ncols=2)
+        fix, ((cpu_plot, open_files_plot), (memory_plot, threads_plot), (load1m_plot, load5m_plot)) = plt.subplots(figsize=(24,16), nrows=3, ncols=2);
 
-        axs[0, 0].plot(plot_out["now"], list(map(float, plot_out["cpu"])), 'tab:orange')
-        axs[0, 0].set_title('CPU')
-        axs[0, 1].plot(plot_out["now"], list(map(float, plot_out["lsof"])), 'tab:red')
-        axs[0, 1].set_title('Open Files')
-        axs[1, 0].plot(plot_out["now"], list(map(float, plot_out["mem"])), 'tab:orange')
-        axs[1, 0].set_title('Memory')
-        axs[1, 1].plot(plot_out["now"], list(map(float, plot_out["threads"])), 'tab:red')
-        axs[1, 1].set_title('Threads')
-        axs[2, 0].plot(plot_out["now"], [item[0] for item in map(lambda x: x.split(), plot_out["load"])], 'tab:orange')
-        axs[2, 0].set_title('Load 1 Minute avg')
-        axs[2, 1].plot(plot_out["now"], [item[1] for item in map(lambda x: x.split(), plot_out["load"])], 'tab:red')
-        axs[2, 1].set_title('Load 5 Minute avg')
+        tab_color = "tab:red"
+        load_1mavg = [item[0] for item in map(lambda x: x.split(), plot_out["load"])]
+        load_5mavg = [item[1] for item in map(lambda x: x.split(), plot_out["load"])]
 
-        for ax in axs.flat:
-            ax.set(xlabel='Time')
+        draw_subplot(plot=cpu_plot, color=tab_color, x=plot_out["now"], y=plot_out["cpu"], ylabel="CPU", xlabel="Time");
+        draw_subplot(plot=open_files_plot, color=tab_color, x=plot_out["now"], y=plot_out["lsof"], ylabel="Open Files", xlabel="Time");
+        draw_subplot(plot=memory_plot, color=tab_color, x=plot_out["now"], y=plot_out["mem"], ylabel="Memory", xlabel="Time");
+        draw_subplot(plot=threads_plot, color=tab_color, x=plot_out["now"], y=plot_out["threads"], ylabel="Threads", xlabel="Time");
+        draw_subplot(plot=load1m_plot, color=tab_color, x=plot_out["now"], y=load_1mavg, ylabel="Load 1 Minute avg", xlabel="Time");
+        draw_subplot(plot=load5m_plot, color=tab_color, x=plot_out["now"], y=load_5mavg, ylabel="Load 5 Minute avg", xlabel="Time");
+       
 
         plt.savefig("{}/{}.svg".format(dirpath, pid), format='svg', dpi=1200, bbox_inches='tight')
 
